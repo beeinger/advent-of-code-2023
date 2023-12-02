52c53856715872b81493441994d2134c3bf1998f
beeinger <sulecki.maciek@gmail.com>
2023-12-02 12:16:25 +0000

    setup

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+mod day_1;
+mod day_2;
+
+fn main() {
+    {
+        use day_1::{part_1, part_2};
+
+        println!(
+            "Day 1, part 1 answer: {}",
+            part_1::sum_calibration(&part_1::get_input())
+        );
+        println!(
+            "Day 1, part 2 answer: {}",
+            part_2::sum_calibration(&part_1::get_input())
+        );
+    }
+
+    {
+        use day_2::{part_1, part_2};
+
+        println!(
+            "Day 2, part 1 answer: {}",
+            part_1::sum_possible_ids(&part_1::get_input())
+        );
+
+        println!(
+            "Day 2, part 2 answer: {}",
+            part_2::sum_powers_of_min_sets(&part_1::get_input())
+        );
+    }
+}
